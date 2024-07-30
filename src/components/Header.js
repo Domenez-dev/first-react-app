2e7cb5f2135613ea2bd3fe113280b7e60f7416b3
Zakkye <126860751+Domenez25@users.noreply.github.com>
2024-07-30 14:19:38 +0200

    removed header for now

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,18 +4,17 @@ function Header() {
   return (
     <div className="header">
       <div className="container">
-        <h1>Welcome to My Landing Page</h1>
         <nav>
-          <ul>
+          <a href="#">Domenez_dev</a>
+          <ul class="list">
             <li><a href="#home">Home</a></li>
             <li><a href="#about">About</a></li>
             <li><a href="#services">Services</a></li>
             <li><a href="#contact">Contact</a></li>
-          </ul>
+          </ul> 
         </nav>
       </div>
     </div>
   );
 }
-
 export default Header;
